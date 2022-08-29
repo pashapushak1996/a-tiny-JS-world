24929648c0978375f98c687936aaf893da2244c7
pashapushak1996 <pashapushak1996@gmail.com>
2022-08-29 22:05:41 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's GitHub pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 class Inhabitant {
-    constructor(species, name, gender, greeting, legs) {
-        this.species = species;
+    constructor(name, gender, greeting) {
+        this.species = this.constructor.name.toLowerCase();
         this.name = name;
         this.gender = gender;
         this.greeting = greeting;
-        this.legs = legs;
     }
 }
 
 class Human extends Inhabitant {
-    constructor(name, gender, greeting, legs, hands) {
-        super(name, gender, greeting, legs);
-        this.species = 'human';
-
-        this.hands = hands;
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+        this.legs = 2;
+        this.hands = 2;
     }
 }
 
-const dog = new Inhabitant('dog', 'Tom', 'male', 'Howl!', 4);
-const cat = new Inhabitant('cat', 'Kitty', 'female', 'Meow!', 4);
-const woman = new Human('Jessy', 'female', 'Hello!', 2, 2);
-const man = new Human('Ted', 'male', 'Hello my dear!', 2, 2);
+class Animal extends Inhabitant {
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+        this.paws = 4;
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+    }
+}
+
+const dog = new Dog('Tom', 'male', 'Howl!');
+const cat = new Cat('Kitty', 'female', 'Meow!');
+const woman = new Human('Jessy', 'female', 'Hello!');
+const man = new Human('Ted', 'male', 'Hello my dear!');
 
 const arrayOfInhabitants = [dog, cat, woman, man];
 
-const arrayOfKeys = ['species', 'name', 'gender', 'greeting', 'legs', 'hands'];
+const arrayOfKeys = ['species', 'name', 'gender', 'greeting', 'legs', 'hands', 'paws'];
 
 const pullValuesOfObj = (obj) => {
     const array = [];
 
     arrayOfKeys.forEach((key) => {
-        obj[key] && array.push(obj[key])
+        obj[key] && array.push(obj[key]);
     });
+
 
     return array;
 };
-
 
 arrayOfInhabitants.forEach((inhabitant,) => {
     const arrayOfValues = pullValuesOfObj(inhabitant);
@@ -53,25 +61,3 @@ arrayOfInhabitants.forEach((inhabitant,) => {
 
     print(stringToPrint);
 });
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
